6f8b4b08dcb7f9ccb8613d99dbdd7803906635b5
Trystan Hendricks <dolkhound@gmx.com>
2022-01-16 12:26:14 -0500

    removed walking projectile now gets thrown at mouse position

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+export (int) var speed: int = 20
+
+var walkVelocity: = Vector2()
+ 
 onready var _animated_sprite = $AnimatedSprite
 
 func shoot() -> void:
@@ -9,17 +13,16 @@ func shoot() -> void:
 
 func _process(delta: float) -> void:
 	var axisX = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	walkVelocity = Vector2()
 	
 	if Input.is_action_pressed("shoot"):
 		shoot()
 	# Movement
-	if Input.is_action_pressed("ui_left"):
-		_animated_sprite.play("Walk")
-		_animated_sprite.flip_h = false
-	elif Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("Walk")
-		_animated_sprite.flip_h = true
-	elif Input.is_action_pressed("attack_activate"):
+	if Input.is_action_pressed("attack_activate"):
 		_animated_sprite.play("Set_Attack")
 	else:
 		_animated_sprite.play("Idle")
+	#walkVelocity = walkVelocity.normalized() * speed
+	
+	#walkVelocity = move_and_slide(walkVelocity)
+
--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -2,8 +2,16 @@ extends KinematicBody2D
 
 var velocity: = Vector2()
 
+var speed: int = 10
+var movement: = Vector2()
+onready var mouse_pos = null
+
+
 func _ready() -> void:
-	velocity.y = -600
+	mouse_pos = get_local_mouse_position()
 
 func _physics_process(delta: float) -> void:
-	move_and_slide(velocity)
+	movement = movement.move_toward(mouse_pos, delta)
+	movement = movement.normalized() * speed
+	position = position + movement
+
