4b534184ad110da3ad18774d8610eb912de67e94
Trystan Hendricks <dolkhound@gmx.com>
2022-01-15 21:05:21 -0500

    working on countdown scene

---
+++ Scenes/Countdown.gd
@@ -0,0 +1,19 @@
+extends Control
+
+#export (int) var minutes = 0
+export (int) var seconds = 0
+var dsec: float = 0
+
+func _physics_process(delta: float) -> void:
+	if seconds > 0 and dsec <= 0:
+		seconds -= 1
+		dsec = 10
+	
+	if seconds >= 10:
+		$sec.set_text(str(seconds))
+	else:
+		$sec.set_text("0"+str(seconds))
+
+
+func _on_Timer_timeout() -> void:
+	dsec -=1
---
+++ Scenes/World.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func _on_CountdownTimer_timeout() -> void:
+	pass # Replace with function body.
