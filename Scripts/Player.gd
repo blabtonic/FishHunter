88066013ca25c90499afd69dd7c523194a6af005
Trystan Hendricks <dolkhound@gmx.com>
2022-01-17 19:45:24 -0500

    fish resets the fade out loop

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var walkVelocity: = Vector2()
  
 onready var _animated_sprite = $AnimatedSprite
+var score = 0
 
 func shoot() -> void:
 	var projectile = load("res://Scenes/Projectile.tscn")
@@ -12,6 +13,9 @@ func shoot() -> void:
 func _process(_delta: float) -> void:
 	walkVelocity = Vector2()
 	
+	var PlayerScoreNode = get_parent().get_node("res://Scenes/Score.tscn")
+	#PlayerScoreNode.text = str(score)
+	
 	if Input.is_action_just_pressed("shoot",true) and Input.is_action_pressed("attack_activate"):
 		shoot()
 		_animated_sprite.play("Finish_Attack")
@@ -20,3 +24,6 @@ func _process(_delta: float) -> void:
 		_animated_sprite.play("Set_Attack")
 	else:
 		_animated_sprite.play("Idle")
+
+func _on_Fish_body_entered(_body: Node) -> void:
+	score += 1
